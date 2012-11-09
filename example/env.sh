87b30313ea41f3d4de0df93397759ecd0773764b
Kyle Isom <coder@kyleisom.net>
2012-11-09 10:14:25 -0700

    Fix typo in environment variables.

--- example/env.sh
+++ example/env.sh
@@ -1,6 +1,6 @@
 # test environment set up
 
-export SSLKEY="$(pwd)/ssl-keys/test.key"
-export SSLCERT="$(pwd)/ssl-keys/test.cert"
+export SSL_KEY="$(pwd)/ssl-keys/test.key"
+export SSL_CERT="$(pwd)/ssl-keys/test.cert"
 export SERVER_ADDR="127.0.0.1"
 export SERVER_PORT="8080"
